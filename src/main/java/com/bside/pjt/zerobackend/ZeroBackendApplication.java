9b001652b9c67f8bf0e918d8e8a3f8ead25eb0ce
yoonjeong <cyj199637@gmail.com>
2022-05-08 22:35:49 +0900

    init commit

---
+++ src/main/java/com/bside/pjt/zerobackend/ZeroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bside.pjt.zerobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bside/pjt/zerobackend/ZeroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bside.pjt.zerobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
