c7aa8482e4ddefe45147706a6095493773cc8756
yoonjeong <cyj199637@gmail.com>
2022-08-21 23:47:52 +0900

    <refactor> qa -> test 로 환경 이름 변경 / 민감정보는 마스킹하여 로깅하도록 수정 / 내 미션 현황보기 API에 미션 카테고리 필드 추가

--- src/main/java/com/bside/pjt/zerobackend/mission/service/dto/MissionProgressDto.java
+++ src/main/java/com/bside/pjt/zerobackend/mission/service/dto/MissionProgressDto.java
@@ -1,5 +1,6 @@
 package com.bside.pjt.zerobackend.mission.service.dto;
 
+import com.bside.pjt.zerobackend.mission.domain.MissionCategory;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,12 +13,14 @@ import lombok.ToString;
 public final class MissionProgressDto {
 
     private final Long missionProgressId;
+    private final MissionCategory missionCategory;
     private final String missionTitle;
     private final Integer progressOrder;
     private final Boolean isCompleted;
 
-    public MissionProgressDto(final String missionTitle, final int progressOrder) {
+    public MissionProgressDto(final MissionCategory missionCategory, final String missionTitle, final int progressOrder) {
         this.missionProgressId = 0L;
+        this.missionCategory = missionCategory;
         this.missionTitle = missionTitle;
         this.progressOrder = progressOrder;
         this.isCompleted = false;
